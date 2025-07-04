399ea685b5aa43ac8e7a1ec9598580e6bd99aeb0
FoamAngel32 <bilibili_laiabao@126.com>
2025-07-04 18:24:38 +0800

    Add files via upload

---
+++ art/bird/1/do.bat
@@ -0,0 +1,7 @@
+G:\gbdk\bin\png2asset b0.png
+G:\gbdk\bin\png2asset ba32.png
+G:\gbdk\bin\png2asset ba64.png
+G:\gbdk\bin\png2asset ba96.png
+G:\gbdk\bin\png2asset bu32.png
+G:\gbdk\bin\png2asset bu64.png
+G:\gbdk\bin\png2asset bu96.png
